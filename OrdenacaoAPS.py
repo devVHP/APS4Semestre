acef756edc4e87a6c657734e724101fc57e2e0b6
devVHP <dev.penga@gmail.com>
2024-10-23 20:58:43 -0300

    Função para pegar informações do PC

--- OrdenacaoAPS.py
+++ OrdenacaoAPS.py
@@ -1,5 +1,21 @@
 #FUNÇÕES
-
+import platform
+import psutil
+def get_system_info():
+    os_name = platform.system()
+    os_version = platform.release()
+    total_memory = psutil.virtual_memory().total / (1024.0 ** 3)
+    cpu_count = psutil.cpu_count()
+    cpu_freq = psutil.cpu_freq().current
+    return {
+        "SO": f"{os_name} {os_version}",
+        "RAM": f"{total_memory:.2f} GB",
+        "Cores": cpu_count,
+        "Frequência": f"{cpu_freq} MHz"
+}
+system_info = get_system_info()
+for key, value in system_info.items():
+    print(f"{key}: {value}")
 #BubbleSort
 #QuickSort
 #InsertionSort
