b621c27a83c19be4952f84bd955e6cfc619f3cdf
devVHP <dev.penga@gmail.com>
2024-10-23 22:48:45 -0300

    bubblesort

--- OrdenacaoAPS.py
+++ OrdenacaoAPS.py
@@ -16,7 +16,20 @@ def get_system_info():
 system_info = get_system_info()
 for key, value in system_info.items():
     print(f"{key}: {value}")
+
 #BubbleSort
+def bubble_sort(lista):
+    n = len(lista)
+    for i in range(n):
+        trocou = False
+        for j in range(0, n-i-1):
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+                trocou = True
+        if not trocou:
+            break
+    return lista
+    
 #QuickSort
 #InsertionSort
 #BinaryInsertionSort
